29e340a2f8ebb26871f7263bb480a630195dc762
Kiridharan <Kiridharan217dec2001@gmail.com>
2024-02-07 21:35:58 +0530

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const isTesting = process.env.ENV === "testing";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isTesting ? "/dragables/" : "/",
+  base: "/dragables/",
 });
